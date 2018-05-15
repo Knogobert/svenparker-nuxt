aa80c86be2edc8e38369e8ac55c4cb2a978e2ff9
Sven Parker <sven.parker@briefcave.se>
2018-05-15 17:26:21 +0200

    Added contact page

---
+++ pages/contact.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="container">
+    <div>
+      <app/>
+      <h1 class="title">
+        Contact
+      </h1>
+      <h2 class="subtitle">
+        Personal portfolio
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="/"
+          class="button--grey">Index</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import App from '~/components/App.vue'
+
+export default {
+  components: {
+    App
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
