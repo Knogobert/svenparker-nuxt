75fd9e7a14745845d4a3c1131992f1188f54782d
Sven Parker <sven.parker@briefcave.se>
2018-05-15 17:26:12 +0200

    Added trivial nav bar

---
+++ components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="nav-main">Nav</nav>
+</template>
+
+<style>
+.nav-main {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  text-align: center;
+  background-color: grey;
+}
+</style>
