6148317a39049f63a0bc9fd7a4481e1e92c9de69
Sven Parker <sven.parker@briefcave.se>
2018-05-24 11:46:56 +0200

    Added basics for new component sp-logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,40 @@
 <template>
-  <div>
+  <div :style="{ 'background-color': coloring.base.lighten }">
     <!-- <sp-nav/> -->
+    <sp-logo/>
     <nuxt/>
   </div>
 </template>
 
 <script>
 import spNav from '~/components/Nav.vue'
+import spLogo from '~/components/Logo.vue'
 
 export default {
   components: {
-    spNav
+    spNav,
+    spLogo
+  },
+  computed: {
+    coloring() {
+      return {
+        base: {
+          default: 'red',
+          lighten: 'pink'
+        }
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
 @import '~assets/_variables.scss';
+
+
+body {
+  background-color: $ufo;
+}
 
 .container {
   min-height: 100vh;
