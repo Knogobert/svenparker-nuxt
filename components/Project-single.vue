4f549b75d1159128f82c6e9ed1e73802b7a8642c
Sven Parker <sven.parker@briefcave.se>
2018-05-29 11:59:26 +0200

    Updated projects to use v-for

--- components/Project-single.vue
+++ components/Project-single.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <div class="project-single">
-    <h3><slot name="title"></slot></h3>
-    <h5><slot name="date"></slot></h5>
-    <p><slot name="description"></slot></p>
+  <div class="project-single project-single--left">
+    <h3 class="project-single-title"><slot name="title"></slot></h3>
+    <h5 class="project-single-subtitle"><slot name="date"></slot></h5>
+    <p class="project-single-description"><slot name="description"></slot></p>
   </div>
 
 </template>
@@ -17,7 +17,15 @@
 
 .project-single {
   background-color: hsla(0, 0%, 0%, .1);
+  height: 400px;
   padding: 50px;
+
+  &--left{
+    text-align: left;
+  }
+  &--right{
+    text-align: right;
+  }
 
   h3,
   h4,
--- components/Projects.vue
+++ components/Projects.vue
@@ -3,22 +3,10 @@
   <section class="sp-projects">
     <h2>Projects</h2>
 
-    <sp-project-single>
-      <template slot="title">La Lepre Stanca</template>
-      <template slot="date">May 2016</template>
-      <template slot="description">During my internship at Grand Public I had the honors to create the Wordpress theme for a bicycling club based in Jönköping. Some of the difficulties were to code a back-end for the site users to manage their meet-ups through. The result was great!</template>
-    </sp-project-single>
-
-    <sp-project-single>
-      <template slot="title">Bankeryds Tennisklubb</template>
-      <template slot="date">April 2016</template>
-      <template slot="description">Website I programmed using SCSS and Wordpress for a tennis club in Bankeryd, Sweden. The back-end uses Advanced Custom Fields Pro so the customer easily can change what's on the front-end. This project took around 100 hours to create, together with Mattias Haal &amp; Mattias Rydh at Grand Public.</template>
-    </sp-project-single>
-
-    <sp-project-single>
-      <template slot="title">Swiss+style</template>
-      <template slot="date">February 2016</template>
-      <template slot="description">A small and very hastily made website as a school exercise. The assignment was to create a "Swiss style" website without using any images. Built using SCSS & PHP.</template>
+    <sp-project-single v-for="project in projects" :key="project">
+      <template slot="title">{{ project.title }}</template>
+      <template slot="date">{{ project.date }}</template>
+      <template slot="description">{{ project.description }}</template>
     </sp-project-single>
 
   </section>
@@ -32,6 +20,27 @@ export default {
   name: 'sp-projects',
   components: {
     spProjectSingle
+  },
+  data() {
+    return {
+      projects: {
+        lls: {
+          title: 'La Lepre Stanca',
+          date: 'May 2016',
+          description: 'During my internship at Grand Public I had the honors to create the Wordpress theme for a bicycling club based in Jönköping. Some of the difficulties were to code a back-end for the site users to manage their meet-ups through. The result was great!'
+        },
+        btk: {
+          title: 'Bankeryds Tennisklubb',
+          date: 'April 2016',
+          description: 'Website I programmed using SCSS and Wordpress for a tennis club in Bankeryd, Sweden. The back-end uses Advanced Custom Fields Pro so the customer easily can change what’s on the front-end. This project took around 100 hours to create, together with Mattias Haal &amp; Mattias Rydh at Grand Public.'
+        },
+        swss: {
+          title: 'Swiss+style',
+          date: 'February 2016',
+          description: 'A small and very hastily made website as a school exercise. The assignment was to create a "Swiss style" website without using any images. Built using SCSS & PHP.'
+        }
+      }
+    }
   }
 }
 </script>
