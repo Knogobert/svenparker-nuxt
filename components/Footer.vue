67512229a90b185aace9368873d8dbe116f45359
Sven Parker <sven.parker@briefcave.se>
2018-06-26 00:21:07 +0200

    Added buggy font weight and width sliders to footer as a påskägg

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,59 @@
 <template>
   <footer class="footer-main" v-bodyZoom:up>
-    <h6>That’s it folks!</h6>
+    <transition name="page">
+      <div class="footer-slider footer-slider--left" v-if="showSliders">
+        <input type="range" min="20" max="188" v-model="sliderWeightValue" @change="changeWeight" class="slider">
+      </div>
+    </transition>
+
+    <h6 @click="toggleShowSliders()">That’s it folks!</h6>
+    
+    <transition name="page">
+      <div class="footer-slider footer-slider--right" v-if="showSliders">
+        <input type="range" min="300" max="500" v-model="sliderWidthValue" @change="changeWidth" class="slider">
+      </div>
+    </transition>
+    <style v-if="sliderChanged">
+      html {
+        font-variation-settings: "wght" {{ weight }}, "wdth" {{ width }};
+      }
+    </style>
   </footer>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      showSliders: false,
+      sliderChanged: false,
+      sliderWeightValue: 110,
+      sliderWidthValue: 420,
+      weight: 110,
+      width: 420
+    }
+  },
+  computed:{
+    computedWeight(){
+      return this.weight;
+    },
+    computedWidth(){
+      return this.width;
+    },
+  },
+  methods:{
+    toggleShowSliders(){
+      this.showSliders = !this.showSliders
+    },
+    changeWeight(){
+      this.sliderChanged = true
+      this.weight = parseInt(this.sliderWeightValue)
+    },
+    changeWidth(){
+      this.sliderChanged = true
+      this.width = parseInt(this.sliderWidthValue)
+    }
+  },
   directives: {
     bodyZoom: {
       inViewport (el) {
@@ -52,6 +100,58 @@ export default {
 
   // for .js-zoom--up on body
   margin-top: -50px;
+
+  h6 {
+    line-height: 1.2;
+    height: 21px;
+    font-variation-settings: "wght" 110, "wdth" 420;
+    transition: font-variation-settings .2s ease-in-out;
+
+    &:active, 
+    &:hover, 
+    &:focus {
+      font-variation-settings: "wght" 140, "wdth" 380;
+      transition: font-variation-settings .2s ease-in-out;
+    }
+  }
+}
+
+.footer-slider {
+  
+  &--left {
+    padding-right: 15px;
+  }
+  &--right {
+    padding-left: 15px;
+  }
+}
+
+.slider {
+  -webkit-appearance: none;  /* Override default CSS styles */
+  appearance: none;
+  background: $ufo-d2; /* Grey background */
+  outline: none; /* Remove outline */
+  border-radius: 2px;
+  padding: 2px 10px;
+}
+
+/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */ 
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  width: 15px; /* Set a specific slider handle width */
+  height: 15px; /* Slider handle height */
+  background: $ufo-l2; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+  border-radius: 50%;
+}
+
+.slider::-moz-range-thumb {
+  width: 15px; /* Set a specific slider handle width */
+  height: 15px; /* Slider handle height */
+  background: $ufo-l2; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+  border-radius: 50%;
 }
 
 </style>
