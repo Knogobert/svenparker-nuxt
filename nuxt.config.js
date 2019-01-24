2427a90fb01a4de2c07abe830b3b643bc901477e
Sven Parker <sven.parker@briefcave.se>
2019-01-24 23:00:43 +0100

    Added style fixes for "The Notch"

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,10 @@ module.exports = {
     title: 'Sven Parker',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, viewport-fit=cover'
+      },
       {
         hid: 'description',
         name: 'description',
